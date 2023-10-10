d62992e9c0a88151fc2e97d86a5c191727571225
sailasroy <sailasroy123@gmail.com>
2023-10-10 14:10:27 +0530

    adding

--- roboshop-infra-standard/01-vpc/main.tf
+++ roboshop-infra-standard/01-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "https://github.com/sailasroy/terraforrm-vpc-advanced.git"
+    source = "git::https://github.com/sailasroy/terraforrm-vpc-advanced.git"
     project_name = var.project_name
     cidr_block = var.cidr_block
     common_tags = var.common_tags
